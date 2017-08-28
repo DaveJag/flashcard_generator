e27025fed87c13be7f401f629fd06759fb35ad77
DaveJag <davejag@comcast.net>
2017-08-27 20:10:13 -0400

    Finished and cleaned up. Everything is working!

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,25 +1,23 @@
-//ClozeCard.js file
+//Dave Jagodowski - HW09 - Building Flashcards with Constructors
 
-//This file contains  the Cloze Constructor which accepts  the full text string and the Cloze-deletion phrase.
+//This function accepts the full text string and the Cloze-deletion phrase.
 
 function ClozeCard(text, cloze){
-
-    //Test input for Cloze-deletion validity by making sure cloze-deletion string is contained in full string. .  
 	//Convert both strings to lower case for comparison. 
 	var lcText = text.toLowerCase();
 	var lcCloze = cloze.toLowerCase();
+
+    //Test input by making sure cloze-deletion string is contained in full string. .  
 	if (!lcText.includes(lcCloze)) {
 		console.log("ERROR:");
 		console.log ("The cloze-deletion string " + "\"" + cloze +"\"" + "is not contained within the full text string.");
 		return;
 	}    
 	else {
-	    this.full = text;//The full statement.
-	    this.cloze = cloze;// the text to be removed from the full text.
-	    this.partial = text.replace(cloze, "_____________"); //the text remaining after removal of the Cloze text.
-	    this.logCloze(this.partial, this.cloze);
-	 }//end else
-};//end function ClozeCard
-
+	    this.full = text;  //The full statement.
+	    this.cloze = cloze;  // the text string to be removed.
+	    this.partial = text.replace(cloze, "__________"); //the Cloze statement.
+	 }
+};
 	
 module.exports = ClozeCard;
--- main.js
+++ main.js
@@ -1,112 +1,209 @@
-//main.js file
+//Dave Jagodowski - HW09 - Making Basic Flashcards and Cloze Deletions using Constructors.
 
 var fs = require("node-fs");
 var request = require("request");
 var inquirer = require("inquirer");
-
 var BasicCard = require("./Basic_Card.js");
 var ClozeCard = require("./ClozeCard.js");
+var cardType = "";
 
 
 
-//collect node input
+function getUserInput() {
+//Find out if user wants to create or view basic/Cloze flashcards.
+inquirer.prompt([
+  {
+    type: "list",
+    name: "doWhat",
+    message: "What would you like to do?",
+    choices: ["Create Basic flashcards", "Create Cloze flashcards", "View the Basic Cards", "View the Cloze Cards", "Exit"]
+  }
+]).then(function(user) {
+	if (user.doWhat === "Create Basic flashcards") {
+		basicCardInfo();
+	}
+	else if (user.doWhat === "Create Cloze flashcards") {
+		clozeCardInfo();
+	}
+	else if (user.doWhat === "View the Basic Cards") {
+		cardType = "basic";
+		showCards(cardType);
+	}
+	else if (user.doWhat === "View the Cloze Cards") {
+		cardType = "cloze";
+		showCards(cardType);
+	}
+	else {
+		console.log("Have a nice day!");
+		return;
+	}
 
-var flashCardType = process.argv[2].toLowerCase();
+})
+};  
 
-//log an error to the console if file cannot be read
-//console.log("flashCardType = " + flashCardType);
-if (flashCardType === "basic") {
- 	fs.readFile("Basic_Card.js", "utf8", function(error, data) {
-  	if (!error) {
-  		var flashcard = new BasicCard("Who was the first president of the United States?", "George Washington");
-  		console.log(flashcard.front);
-  		console.log(flashcard.back);
+
+function basicCardInfo() {
+//Gathers data for a new basic card
+	inquirer.prompt([
+  {
+    type: "input",
+    name: "front",
+    message: "Enter a question for the front of the flashcard: "
+  },
+  {
+    type: "input",
+    name: "back",
+    message: "Enter the answer for the back of the flashcard: "
+   }
+   ]).then(function(cardInfo) {
+   	    newBasic(cardInfo.front, cardInfo.back);
+   	});
+};
+
+
+
+function clozeCardInfo() {
+//Gathers date for a new cloze card
+	inquirer.prompt([
+  {
+    type: "input",
+    name: "front",
+    message: "Enter a full statement for the front of the Cloze flashcard: "
+  },
+  {
+    type: "input",
+    name: "back",
+    message: "Enter the Cloze-deletion phrase to remove. (Must exactly match part of the full statement.): "
+   }
+   ]).then(function(cardInfo) {
+   	    newCloze(cardInfo.front, cardInfo.back);
+   	});
+};
+
+
+
+function createAnotherBasicCard() {
+  //Prompts user to create an additional Basic flashcard
+	inquirer.prompt([
+    {
+        type: "confirm",
+        name: "another",
+        message: "Create another card?"
+    }
+    ]).then(function(result) {
+        if (result.another) {
+        	  basicCardInfo();
+        }  else {
+        	   getUserInput();
+           }
+    })       
+};
+
+
+
+function createAnotherClozeCard() {
+   //Prompts user to create an additional Cloze flashcard
+	inquirer.prompt([
+    {
+        type: "confirm",
+        name: "another",
+        message: "Create another Cloze card?"
+    }
+    ]).then(function(result) {
+        if (result.another) {
+        	  clozeCardInfo();
+        } else {
+        	    getUserInput();
+           }
+    })       
+};
+
+
+
+function newBasic(question, answer){  
+	fs.readFile("Basic_Card.js", "utf8", function(error, data) {
+  	if (error) {
+    	console.log(error);
+    	return;
+    }
+  	else {  //Create the flashcard using constructor in Basic_Card.js file.
+	      var flashcard = new BasicCard(question, answer);
+        //format flashcard info and write it to log file
         var logBasicOutput = ("Question: " + flashcard.front + "; " + "\r\n" + "Answer: " + flashcard.back + ";" + "\r\n"); 
-  		fs.appendFile("logbasic.txt", logBasicOutput, "utf8", function(error) {
-        	if (error) {
-        		console.log(error);
-        	}//end if
-        showCards();
-        })//end fs.appenfile function
-
-	} else {
-		console.log(error);
-	    return; 
+	      fs.appendFile("logbasic.txt", logBasicOutput, "utf8", function(error) {
+      	    if (error) {
+   		          console.log(error);
+   		          return;
+   	        }//end if error
+   	        else {
+	              createAnotherBasicCard();
+	          } 
+	      })
 	  }
-	})//end fs.readFile
-}//end if
+	})    
+}; 
 
-else if (flashCardType === "cloze") {
+
+
+
+function newCloze(text, cloze) {
 	fs.readFile("ClozeCard.js", "utf8", function(error, data) {
-  	if (!error) {
-  	    var clozecard = new ClozeCard("George Washington was the first president of the United States.", "George Washington");
-        console.log(clozecard.partial);
-  	    console.log(clozecard.cloze); 
-  	    var logClozeOutput = ("Question: " + clozecard.partial + "; \r\n" + "Answer: " + clozecard.cloze + "\r\n"); 
-  		fs.appendFile("logcloze.txt", logClozeOutput, "utf8", function(error) {
-        	if (error) {
-        		console.log(error);
-        	}//end if
-        })//end fs.appenfile function
-
-	} else {
-		console.log(error);
-	    return; 
-	  }
-	})//end fs.readFile
-}//end function 
-
-else {
-	  	console.log("Sorry, that command is not valid. Please enter one of the following words:");
-	  	console.log("Enter \"basic\" for a simple two-sided flash card.");
-	  	console.log("Enter \"cloze\" for a cloze-deleted flashcard that contains partial text.");
-	  }	//end else
-
-
-
-//var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
+  	  if (error) {
+    	    console.log(error);
+    	    return;
+      }
+  	  else { //create new Cloze flashcard with constructor
+  	      var clozecard = new ClozeCard(text, cloze);
+  	      //format card data and print to cloze log file.
+  	      var logClozeOutput = ("Question: " + clozecard.partial + "; \r\n" + "Answer: " + clozecard.cloze + "\r\n"); 
+  		    fs.appendFile("logcloze.txt", logClozeOutput, "utf8", function(error) {
+       	      if (error) {
+   		            console.log(error);
+   		            return;
+   	          }
+   	          else {
+	                createAnotherClozeCard();
+	            } 
+          })
+	    }
+	})
+};
 
 
 
 
-//Create a new basic card
-function newBasic(){  
-	console.log("Enter question for front of the card.");
-	var question = process.argv[0].toLowerCase();
-	console.log("question is " + question);
-}
+function showCards(cardType) {
+    //Displays existing cards from the log files, based on the user's selection. 
+    if (cardType === "basic") {
+    	  logfile = "./logbasic.txt";
+    }
+    else {
+      	logfile = "./logcloze.txt";
+    }
 
-
-
-function isEven(value) {
-	if (value%2 == 0)
-		return true;
-	else
-		return false;
-}
-
-var showCards = function() {
-
-    fs.readFile("./logbasic.txt", "utf8", function(error, data) {
-        //if there is an error, log it
+    fs.readFile(logfile, "utf8", function(error, data) {
         if (error) {
             console.log(error);
         }
+        //read questions and answers from logfile into an array. 
         var questions = data.split("\r\n");
         var notBlank = function(value) {
             return value;
         };
         questions = questions.filter(notBlank);
+        //Even numbered indexes in the array are the Questions. Odd indexes are the Answers.
         var count = 0;
         while (questions[count] != undefined){
-            console.log("output Q " + questions[count]);
-            console.log("output A " + questions[count+1]);
-            count = count + 2;
+            console.log("--------------------------------------" + "\r\n");
+            console.log(questions[count]); //display question
+            console.log(questions[count+1] + "\r\n"); //display answer
+            
+            count = count + 2; //move to then next question
         }
+    //After showing input, display main menu.
+    getUserInput();  //After showing input, display main menu. 
     });
 };
-/*
-var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
-console.log(firstPresident.front);
-console.log(firstPresident.back);
-*/
+
+
+getUserInput();  //Initiates the program.
